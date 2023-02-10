a2cad3326026d2cfab99d6b850b04d33112d6914
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2023-02-10 22:22:01 +0500

    continue

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,73 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // Widget con = Text('Hello');
-  List<Widget> list = [];
+  TextEditingController controller = TextEditingController();
+  List<Map<int, Widget>> list = [];
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    for (int i = 0; i <=100; i++)
-      list.add(ListTile(
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.local_hospital)),
-        title: Text('Name $i'),
-        subtitle: Text('Surname$i'),
-        trailing: Icon(Icons.delete,color: Color.fromARGB(255, 194, 102, 96),),
-      ));
+    // for (int i = 0; i <=5; i++)
+    //   list.add(ListTile(
+    //     leading: IconButton(onPressed: (){
+    //       setState(() {
+    //       if(controller.text!=''){
+    //         list.add(Text());
+    //       }
+
+    //       });
+    //     }, icon: Icon(Icons.local_hospital)),
+    //     title: TextField(controller: controller,),
+    //     subtitle: Text('Surname$i'),
+    //     trailing: Icon(Icons.delete,color: Color.fromARGB(255, 194, 102, 96),),
+    //   ));
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
-        children: list,
+        children: [
+          ListTile(
+            leading: IconButton(
+                onPressed: (() {
+                  setState(() {
+                    if (controller.text != '') {
+                      // list.add(ListTile(
+                      //   leading: IconButton(
+                      //     onPressed: () {},
+                      //     icon: Icon(Icons.add_box_rounded),
+                      //   ),
+                      //   trailing: IconButton(
+                      //       onPressed: () {
+
+                      //       },
+                      //       icon: Icon(Icons.delete)),
+                      //   title: Text(controller.text),
+                      //   subtitle: Text('person\'s name'),
+                      // ));
+                      index += 1;
+                      list.add(
+                        {
+                          index: ListTile(
+                            title: Text(controller.text),
+                            trailing: IconButton(onPressed: (){
+                              list.removeWhere((element) => element.keys.toList()[0] == index);
+                            }, icon: const Icon(Icons.delete),),
+                          ),
+                        },
+                      );
+                    }
+                  });
+                }),
+                icon: Icon(Icons.add_box_rounded)),
+            title: TextField(
+                controller: controller, decoration: InputDecoration()),
+            subtitle: Text('Enter name'),
+            trailing: IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+          ),
+          // Column(
+          //   chil,
+          // )
+        ],
       ),
     );
   }
